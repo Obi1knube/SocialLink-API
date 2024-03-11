decfc8f23bcafb9aac72b77d251b042f9426a10f
Obi1knube <123329625+Obi1knube@users.noreply.github.com>
2024-03-11 23:43:25 +0000

    Controllers completed

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 const { connect, connection } = require("mongoose");
 
 connect("mongodb://127.0.0.1:27017/sociallinkDB");
+connect(connectionString);
 
 module.exports = connection;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,12 @@
+// Import necessary modules from Mongoose
 const { User, Thought } = require("../models");
 
+// Sync Mongoose model with the MongoDB
 User.sync();
 Thought.sync();
 
 const userController = {
+  // GET all users
   getAllUsers: async (req, res) => {
     try {
       const users = await User.find();
@@ -13,11 +16,14 @@ const userController = {
     }
   },
 
+  // GET a single user by its _id and populated thought and friend data
   getUserById: async (req, res) => {
     try {
-      const user = await User.findById(req.params.id)
+      const user = await User.findById(req.params.userId)
         .populate("thoughts")
-        .populate("friends");
+        .populate("friends")
+        .select("-__v")
+        .then(user);
       if (!user) {
         res.status(404).json({ message: "User not found" });
         return;
@@ -28,23 +34,24 @@ const userController = {
     }
   },
 
+  // POST a new user
   createUser: async (req, res) => {
     try {
       const newUser = await User.create(req.body);
       res.status(201).json(newUser);
     } catch (err) {
-      res.status(400).json(err);
+      res.status(500).json(err);
+      console.log(err);
     }
   },
 
+  // PUT to update a user by its _id
   updateUser: async (req, res) => {
     try {
       const user = await User.findOneAndUpdate(
-        { _id: req.params.id },
-        req.body,
-        {
-          new: true,
-        }
+        { _id: req.params.userId },
+        { $set: req.body },
+        { new: true }
       );
       if (!user) {
         res.status(404).json({ message: "User not found" });
@@ -52,20 +59,57 @@ const userController = {
       }
       res.json(user);
     } catch (err) {
-      res.status(400).json(err);
+      res.status(500).json(err);
     }
   },
 
+  // DELETE to remove user by its _id
   deleteUser: async (req, res) => {
     try {
-      const user = await User.findByIdAndDelete(req.params.id);
+      const user = await User.findByIdAndDelete(req.params.userId);
       if (!user) {
         res.status(404).json({ message: "User not found" });
         return;
       }
       res.json(user);
     } catch (err) {
-      res.status(400).json(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // Add a friend to a User's friend list
+  addFriend: async (req, res) => {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.params.friendId } },
+        { new: true }
+      );
+      if (!user) {
+        res.status(404).json({ message: "User not found" });
+        return;
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // Remove a friend from a User's friend list
+  removeFriend: async (req, res) => {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: req.params.friendId } },
+        { new: true }
+      );
+      if (!user) {
+        res.status(404).json({ message: "User not found" });
+        return;
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
     }
   },
 };
